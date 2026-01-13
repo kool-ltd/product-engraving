5a35950b332f7f33eccfc74949258f620070a7fe
francispun <95836367+francispun@users.noreply.github.com>
2026-01-13 17:17:20 +0800

    Delete js directory

--- js/constants.js
+++
@@ -1,7 +0,0 @@
-export const KNIVES = {
-  big: ['santoku', 'chef', 'bread'],
-  small: ['utility', 'paring'],
-  others: ['chopper', 'choppingBoard', 'tongs', 'scissors', 'turner']
-};
-
-export const TOGGLE_DEBOUNCE = 200; // ms
